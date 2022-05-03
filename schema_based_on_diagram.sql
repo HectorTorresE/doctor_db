0543f5e6907495b98dc89f430f332316d00018ca
mengiefen <mengiefen@gmail.com>
2022-05-03 22:41:15 +0300

    add invoices & invoice_items table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -24,10 +24,7 @@ CREATE TABLE treatments (
   id INT GENERATED ALWAYS AS IDENTITY,
   type VARCHAR(50),
   name VARCHAR(100),
-  PRIMARY KEY (id),
-  CONSTRAINT FK_medical_histories_id
-  FOREIGN KEY (id)
-  REFERENCES medical_histories(id)
+  PRIMARY KEY (id) 
 );
 
 DROP TABLE IF EXISTS medical_histories_treatments;
@@ -35,10 +32,41 @@ CREATE TABLE medical_histories_treatments (
   id INT GENERATED ALWAYS AS IDENTITY,
   medical_histories_id INT,
   treatments_id INT,
-  CONSTRAINT FK_medical_histories_id
-  FOREIGN KEY (medical_histories_id)
+  CONSTRAINT FK_medical_history_id
+  FOREIGN KEY (medical_history_id)
   REFERENCES medical_histories(id),
   CONSTRAINT FK_treatments_id
   FOREIGN KEY (treatments_id)
   REFERENCES treatments(id)
 );
+
+DROP TABLE IF EXISTS invoices;
+CREATE TABLE invoices (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT
+  CONSTRAINT FK_invoices_medical_history_id
+  FOREIGN KEY (medical_history_id)
+  REFERENCES medical_histories(id)
+);
+
+
+DROP TABLE IF EXISTS invoice_items;
+CREATE TABLE invoice_items (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT,
+  treatment_id INT,
+  CONSTRAINT FK_invoice_id
+  FOREIGN KEY (invoice_id)
+  REFERENCES invoices(id),
+  CONSTRAINT FK_invoice_treatment_id
+  FOREIGN KEY (treatment_id)
+  REFERENCES treatments(id)
+);
+
+
